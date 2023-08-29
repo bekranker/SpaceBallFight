141dc7749a9554fb910482e0eaa8dd82d85a899e
Bekir <bekirekiz@ogrenci.beykoz.edu.tr>
2023-08-29 09:05:44 +0300

    Last builds

---
+++ Color Curve/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private ButtonClickManager _StartButton, _SettingsButton;
+    [SerializeField] private GameObject _SettingsPanel;
+    [SerializeField] private List<GameObject> _Objects = new List<GameObject>();
+    [SerializeField] private Animator _Animator;
+
+
+
+
+    private void Start()
+    {
+        _StartButton.DoSomething += () => StartCoroutine(StartButton());
+        _SettingsButton.DoSomething += SettingsButton;
+    }
+    private IEnumerator StartButton()
+    {
+        _Animator.SetTrigger("Exit");
+        yield return new WaitForSecondsRealtime(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    private void SettingsButton()
+    {
+        _SettingsPanel.SetActive(true);
+        CloseOrOpen(false);
+    }
+    public void CloseOrOpen(bool state)
+    {
+        _Objects.ForEach((_objects)=>_objects.SetActive(state));
+    }
+}
